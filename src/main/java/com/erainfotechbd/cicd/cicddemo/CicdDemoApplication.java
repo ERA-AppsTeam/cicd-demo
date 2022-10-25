82bef05587ff1fb41f2f51c09e474d2010b8a2a3
enamul-java <enamul.java@gmail.com>
2022-10-25 11:59:03 +0600

    final setup

--- src/main/java/com/erainfotechbd/cicd/cicddemo/CicdDemoApplication.java
+++ src/main/java/com/erainfotechbd/cicd/cicddemo/CicdDemoApplication.java
@@ -2,12 +2,18 @@ package com.erainfotechbd.cicd.cicddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CicdDemoApplication {
+public class CicdDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicdDemoApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CicdDemoApplication.class);
+	}
 }
